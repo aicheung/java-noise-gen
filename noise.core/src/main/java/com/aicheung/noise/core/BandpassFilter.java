b7f842781514b9c2b2735bf2945245deda3d0dc1
Eric Cheung <nelsonct2222@gmail.com>
2023-08-11 19:12:45 +0100

    Multi frequency bands support

--- noise.core/src/main/java/com/aicheung/noise/core/BandpassFilter.java
+++ noise.core/src/main/java/com/aicheung/noise/core/BandpassFilter.java
@@ -1,37 +1,64 @@
 package com.aicheung.noise.core;
 
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import org.jtransforms.fft.DoubleFFT_1D;
 
 public class BandpassFilter {
 
     public double[] filter(double[] inputSignal, double sampleRate, double lowFreq, double highFreq) {
+        return filter(inputSignal, sampleRate, new double[] {lowFreq}, new double[] {highFreq});
+    }
+
+    public double[] filter(double[] inputSignal, double sampleRate, double[] lowFreq, double[] highFreq) {
+        if (lowFreq.length != highFreq.length) {
+            throw new IllegalArgumentException("lowFreq and highFreq arrays must have the same length");
+        }
+        System.out.println(Arrays.toString(lowFreq));
+        System.out.println(Arrays.toString(highFreq));
+    
         DoubleFFT_1D fft = new DoubleFFT_1D(inputSignal.length);
         double[] fftData = new double[inputSignal.length * 2];
-        
+    
         System.arraycopy(inputSignal, 0, fftData, 0, inputSignal.length);
         fft.realForwardFull(fftData);
-        
-        int lowBound = (int) (lowFreq * fftData.length / sampleRate);
-        int highBound = (int) (highFreq * fftData.length / sampleRate);
-        
-        // Zero out undesirable frequencies
-        for (int i = 0; i < lowBound; i++) {
-            fftData[i] = 0;
-            fftData[fftData.length - 1 - i] = 0;
-        }
-        
-        for (int i = highBound; i < fftData.length / 2; i++) {
-            fftData[i] = 0;
-            fftData[fftData.length - 1 - i] = 0;
+    
+        // Initially mark all frequencies for zeroing
+        boolean[] shouldZero = new boolean[fftData.length];
+        Arrays.fill(shouldZero, true);
+
+        // Unmark the desired frequency bands
+        for (int band = 0; band < lowFreq.length; band++) {
+            int lowBound = (int) (lowFreq[band] * fftData.length / sampleRate);
+            int highBound = (int) (highFreq[band] * fftData.length / sampleRate);
+
+            for (int i = lowBound; i <= highBound; i++) {
+                shouldZero[i] = false;
+                if(i + 1 < fftData.length && i == highBound) {
+                    shouldZero[i + 1] = false;
+                }
+            }
         }
 
+        // Zero out the marked frequencies
+        for (int i = 0; i < fftData.length; i++) {
+            if (shouldZero[i]) {
+                fftData[i] = 0;
+                //fftData[2 * i + 1] = 0;
+            }
+        }
+    
         fft.complexInverse(fftData, true);
-        
+    
         double[] filteredSignal = new double[inputSignal.length];
         for (int i = 0; i < filteredSignal.length; i++) {
             filteredSignal[i] = fftData[i * 2];
         }
-
+    
         return filteredSignal;
     }
 }
--- noise.core/src/main/java/com/aicheung/noise/core/NoiseGenerator.java
+++ noise.core/src/main/java/com/aicheung/noise/core/NoiseGenerator.java
@@ -5,21 +5,22 @@ import java.util.Random;
 
 public class NoiseGenerator {
 
-    private static final float SAMPLE_RATE = 44100;
+    private static final float SAMPLE_RATE = 48000;
     private static final int AMPLITUDE = 32760;
     private static final int SAMPLE_SIZE = 16; // in bits
     private static final int CHANNELS = 1; // mono
     private static final boolean SIGNED = true;
     private static final boolean BIG_ENDIAN = false;
     private static final Random RANDOM = new Random();
+    private static final float Q_FACTOR = 1.41f;
 
     private static BandpassFilter filter;
 
     public static void main(String[] args) throws LineUnavailableException {
         filter = new BandpassFilter();
 
-        float frequency = 100.0f; // can be taken from user input
-        byte[] noise = generateFilteredNoise(frequency);
+        float[] frequencies = new float[] {100.0f, 1000.0f}; // can be taken from user input
+        byte[] noise = generateFilteredNoise(frequencies);
 
         AudioFormat format = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE, CHANNELS, SIGNED, BIG_ENDIAN);
         SourceDataLine line = AudioSystem.getSourceDataLine(format);
@@ -31,7 +32,11 @@ public class NoiseGenerator {
     }
 
     public static byte[] generateFilteredNoise(float frequency) {
-        int length = (int) SAMPLE_RATE * 10; // for example, 10 seconds of audio
+        return generateFilteredNoise(new float[] {frequency});
+    }
+
+    public static byte[] generateFilteredNoise(float[] frequencies) {
+        int length = (int) SAMPLE_RATE * 15; // for example, 10 seconds of audio
         byte[] noiseData = new byte[length * 2]; // *2 for 16-bit samples
 
         // Generate white noise
@@ -42,10 +47,31 @@ public class NoiseGenerator {
         }
 
         double[] noise = ByteArrayConverter.byteArrayToDoubleArray(noiseData);
-        double[] filtered = filter.filter(noise, SAMPLE_RATE, frequency-20, frequency+20);
+        double[] filtered = filter.filter(noise, SAMPLE_RATE, getLowFreqByQFactor(frequencies, Q_FACTOR), getHighFreqByQFactor(frequencies, Q_FACTOR));
         noiseData = ByteArrayConverter.doubleArrayToByteArray(filtered);
         return noiseData;
     }
 
-    // Implement the bandpass filter method here
+    public static double[] getLowFreqByQFactor(float[] frequencies, float Q) {
+        double[] lowFreq = new double[frequencies.length];
+    
+        for (int i = 0; i < frequencies.length; i++) {
+            double bandwidth = frequencies[i] / Q;
+            lowFreq[i] = frequencies[i] - bandwidth / 2;
+        }
+    
+        return lowFreq;
+    }
+    
+    public static double[] getHighFreqByQFactor(float[] frequencies, float Q) {
+        double[] highFreq = new double[frequencies.length];
+    
+        for (int i = 0; i < frequencies.length; i++) {
+            double bandwidth = frequencies[i] / Q;
+            highFreq[i] = frequencies[i] + bandwidth / 2;
+        }
+    
+        return highFreq;
+    }
+    
 }
